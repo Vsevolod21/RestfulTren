397e5561705592cf5e06c3fb17ac4e9b01c4e658
Vsevolod21 <kononenko.vsevolod@gmail.com>
2023-11-22 20:37:22 +0300

    Initial commit

---
+++ src/main/java/com/example/restfulltren/RestfullTrenApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulltren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullTrenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullTrenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulltren/RestfullTrenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulltren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullTrenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
